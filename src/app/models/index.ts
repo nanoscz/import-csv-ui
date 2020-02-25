cccbd8f5cae02b07278d978897135e6c9c86124f
fernando <fernandocto.scz@gmail.com>
2020-02-25 17:53:34 -0400

    [ADD] added DataCasit class

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -12,4 +12,17 @@ export class DemographicSocioData {
   public question01: any;
 	public question02: any;
 	public question03: any;
+	public question04: any;
+	public question05: any;
+}
+
+export class DataCasit {
+	public date: any;
+	public parent: any;
+	public ci: any;
+	public fullName: any;
+	public age: any;
+	public gender: any;
+	public course: any;
+	public question01: any;
 }
