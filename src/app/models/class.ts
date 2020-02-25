08f9770b31f5c080b308b15b7de729ba0e8ca7e1
fernando <fernandocto.scz@gmail.com>
2020-02-24 23:09:46 -0400

    [ADD] class DemographicSocioData

---
+++ src/app/models/class.ts
@@ -0,0 +1,15 @@
+export class DemographicSocioData {
+	public date: any;
+	public fullName: any;
+	public age: any;
+	public gender: any;
+	public ci: any;
+	public occupation: any;
+	public study: any;
+	public maritalStatus: any;
+  public economy: any;
+  public numberOfChildren: any;
+  public question01: any;
+	public question02: any;
+	public question03: any;
+}
