12d7b9ad30047af7918c4350cbd4a8be15541d8b
fernando <fernandocto.scz@gmail.com>
2020-02-24 23:21:39 -0400

    [ADD] added settings

---
+++ src/app/config/index.ts
@@ -0,0 +1,19 @@
+export const Settings = {
+  fichas: {
+    demographicSocioHeader: {
+      date: 'Fecha',
+      fullName: 'Nombre',
+      age: 'Edad',
+      gender: 'Genero',
+      ci: 'Cedula de Identidad',
+      occupation: 'Ocupacion',
+      study: 'Formacion',
+      maritalStatus: 'Estado Civil',
+      economy: 'Economia',
+      numberOfChildren: 'Hijos',
+      question01: 'Pregunta 1',
+      question02: 'Pregunta 2',
+      question03: 'Pregunta 3'
+    }
+  }
+}
