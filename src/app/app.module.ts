94c193e92a09a7ad7cf87f365810d9c373cdc208
fernando <fernandocto.scz@gmail.com>
2020-02-25 14:09:07 -0400

    [ADD] added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculateAnxietyPipe } from './pipes/calculate-anxiety.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CalculateAnxietyPipe } from './pipes/calculate-anxiety.pipe';
     CalculateAnxietyPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
