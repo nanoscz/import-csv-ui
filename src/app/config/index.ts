985875571dfd2739374df9795c15e898b2d51c23
fernando <fernandocto.scz@gmail.com>
2020-02-25 01:53:46 -0400

    [ADD] set the questionnaires in the configuration

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -13,7 +13,172 @@ export const Settings = {
       numberOfChildren: 'Hijos',
       question01: 'Pregunta 1',
       question02: 'Pregunta 2',
-      question03: 'Pregunta 3'
+      question03: 'Pregunta 3',
+      points: 'Puntos',
+      anxiety: 'Ansiedad'
     }
   }
 }
+
+export const Questionnaire = [
+  {
+    question1: {
+      code: 1,
+      type: false,
+      index: 13,
+      question: 'Me siento bien',
+    }
+  },
+  {
+    question2: {
+      code: 2,
+      type: true,
+      index: 14,
+      question: 'Me canso rápindexamente'
+    }
+  },
+  {
+    question3: {
+      code: 3,
+      type: true,
+      index: 15,
+      question: 'Siento ganas de llorar'
+    }
+  },
+  {
+    question4: {
+      code: 4,
+      type: true,
+      index: 16,
+      question: 'Quisiera ser tan feliz como otros parecen serlo'
+    }
+  },
+  {
+    question5: {
+      code: 5,
+      type: true,
+      index: 17,
+      question: 'Me pierdo cosas por no poder decindexirme rápindexamente'
+    }
+  },
+  {
+    question6: {
+      code: 6,
+      type: false,
+      index: 18,
+      question: 'Me siento descansado'
+    }
+  },
+  {
+    question7: {
+      code: 7,
+      type: false,
+      index: 19,
+      question: 'Soy una persona “tranquila serena y sosegada'
+    }
+  },
+  {
+    question8: {
+      code: 8,
+      type: true,
+      index: 20,
+      question: 'Siento que las dificultades se me amontonan al punto de no poder superarlas'
+    }
+  },
+  {
+    question9: {
+      code: 9,
+      type: true,
+      index: 21,
+      question: 'Me preocupo demasiado por cosas sin importancia'
+    }
+  },
+  {
+    question10: {
+      code: 10,
+      type: false,
+      index: 22,
+      question: 'Soy feliz '
+    }
+  },
+  {
+    question11: {
+      code: 11,
+      type: true,
+      index: 23,
+      question: 'Tomo las cosas muy a pecho'
+    }
+  },
+  {
+    question12: {
+      code: 12,
+      type: true,
+      index: 24,
+      question: 'Me falta confianza en mí mismo'
+    }
+  },
+  {
+    question13: {
+      code: 13,
+      type: false,
+      index: 25,
+      question: 'Me siento seguro'
+    }
+  },
+  {
+    question14: {
+      code: 14,
+      type: true,
+      index: 26,
+      question: 'Trato de evitar enfrentar una crisis o dificultad'
+    }
+  },
+  {
+    question15: {
+      code: 15,
+      type: false,
+      index: 27,
+      question: 'Me siento melancólico'
+    }
+  },
+  {
+    question16: {
+      code: 16,
+      type: true,
+      index: 28,
+      question: 'Me siento satisfecho'
+    }
+  },
+  {
+    question17: {
+      code: 17,
+      type: true,
+      index: 29,
+      question: 'Algunas indexeas poco importantes pasan por mi cabeza y me molestan'
+    }
+  },
+  {
+    question18: {
+      code: 18,
+      type: true,
+      index: 30,
+      question: 'Me afectan tanto los desengaños que no me los puedo quitar de la cabeza'
+    }
+  },
+  {
+    question19: {
+      code: 19,
+      type: false,
+      index: 31,
+      question: 'Soy una persona estable'
+    }
+  },
+  {
+    question20: {
+      code: 20,
+      type: true,
+      index: 32,
+      question: 'Cuando pienso en los asuntos que tengo entre manos me pongo tenso y alterado'
+    }
+  }
+];
