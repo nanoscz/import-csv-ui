19660c3494051e0304d3d9c870d3e36bfde7173c
fernando <fernandocto.scz@gmail.com>
2020-02-25 01:52:47 -0400

    [ADD] resgiter pipe in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculateAnxietyPipe } from './pipes/calculate-anxiety.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculateAnxietyPipe
   ],
   imports: [
     BrowserModule
